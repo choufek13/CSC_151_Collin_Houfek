03e572b1a8f5658c1c2aa7cc158b30a73b472b6e
choufek13 <houfekcl700@mail.sandhills.edu>
2026-02-02 03:44:27 +0000

    week 2 lab 1

---
+++ src/labs/example/mammal/Mammal.java
@@ -0,0 +1,49 @@
+package labs.example.mammal;
+
+public class Mammal {
+    
+    private String hairColor;
+    private String eyeColor;
+    private double bodytemp;
+
+    private int age;
+    private double weight;
+    private double height;
+
+
+    public void run() {
+        System.out.println("The mammal is running.");
+    }
+
+    public void eat() {
+        System.out.println("The mammal is eating.");
+    }
+
+    public void sleep() {
+        System.out.println("The mammal is sleeping.");
+    }
+    
+    public void scratch() {
+        System.out.println("The mammal is scratching.");
+    }
+
+    public void play() {
+        System.out.println("The mammal is playing.");
+    }
+
+    public void protect() {
+        System.out.println("The mammal is protecting.");
+    }
+
+    public void walk() {
+        System.out.println("The mammal is walking.");
+    }
+
+    public void dig() {
+        System.out.println("The mammal is digging.");
+    }
+
+    public void smile() {
+        System.out.println("The mammal is smiling.");
+    }
+}

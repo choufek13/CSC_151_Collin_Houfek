92597a4770ff2f1d3ab887f22aa108252c7fa8ee
choufek13 <houfekcl700@mail.sandhills.edu>
2026-02-08 16:29:47 -0500

    Week 4 Lab

--- src/labs/example/mammal/Mammal.java
+++ src/labs/example/mammal/Mammal.java
@@ -4,7 +4,7 @@ public class Mammal {
     
     private String hairColor;
     private String eyeColor;
-    private double bodytemp;
+    private double bodyTemp;
 
     private int age;
     private double weight;
@@ -45,5 +45,39 @@ public class Mammal {
 
     public void smile() {
         System.out.println("The mammal is smiling.");
+    }
+    public Mammal (String haircolor, String eyeColor, double bodyTemp, int age, double weight, double height) {
+
+        this.hairColor = hairColor;
+        this.eyeColor = eyeColor;
+        this.bodyTemp = bodyTemp;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;    
+    }
+
+    public void getMammalDetails() {
+        System.out.println("Hair Color: " + hairColor);
+        System.out.println("Eye Color: " + eyeColor);
+        System.out.println("Body Temperature: " + bodyTemp);
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+        System.out.println("Height: " + height);
+    }
+
+    public void sit() {
+        System.out.println("The mammal is sitting.");
+
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedExpectation e) {
+            System.out.println("Sitting was interrupted.");
+        }
+
+        stand();
+    }
+
+    public void stand() {
+        System.out.println("The mammal is now standing and barking");
     }
 }

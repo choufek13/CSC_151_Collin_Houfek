f3154dadde5dd3b0782adea072f57dffdc7e4631
choufek13 <houfekcl700@mail.sandhills.edu>
2026-02-15 23:34:05 -0500

    week 5 lab

--- src/labs/example/physics/Main.java
+++ src/labs/example/physics/Main.java
@@ -12,12 +12,21 @@ public class Main {
     public static void main(String[] args) {
         Physics physics = new Physics();
         
-        double distance = physics.getDistance();
-        double velocity = physics.getVelocity();
-        double momentum = physics.getMomentum();
-        double force = physics.getForce();
-        double work = physics.getWork();
-        double KineticEnergy = physics.getKineticEnergy();
-        double PotentialEnergy = physics.getPotentialEnergy();
+        double distance = physics.getDistance(10, 2);
+        double velocity = physics.getVelocity(5, 3);
+        double momentum = physics.getMomentum(4, 9);
+        double force = physics.getForce(10, 6);
+        double work = physics.getWork(12, 4);
+        double KineticEnergy = physics.getKineticEnergy(8, 2);
+        double PotentialEnergy = physics.getPotentialEnergy(9, 3);
+    
+//print return value with proper unit
+    System.out.println("Distance:" + distance + "miles" );
+    System.out.println("Velocity:" + velocity + "mph");
+    System.out.println("Momentum:" + momentum + "kg m/s");
+    System.out.println("Force:" + force + "kg m/s * s");
+    System.out.println("Work:" + work + "Joules");
+    System.out.println("Kinteic Energy:" + kineticEnergy + "Joules");
+    System.out.println("Potential Energy:" + potentialEnergy + "Joules");
     }
 }

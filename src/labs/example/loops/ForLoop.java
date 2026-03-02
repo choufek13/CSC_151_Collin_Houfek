a05007303bf820c1f250f91ac3c639b98eea8966
choufek13 <houfekcl700@mail.sandhills.edu>
2026-03-02 01:54:07 +0000

    week 7 lab changes

--- src/labs/example/loops/ForLoop.java
+++ src/labs/example/loops/ForLoop.java
@@ -10,24 +10,40 @@ package labs.example.loops;
 public class ForLoop {
     public static void main(String[] args) {
        executeForLoop();
-       sumTwoNumbers();
-       sumLoopCounter();
+       sumTwoNumbers(3,7);
+       sumLoopCounter(10);
        printMultiplicationTable();
-
     }
 
+//private loop increments, loop 20 times
     private static void executeForLoop() {
-        int number = 1;
-        for (number = 1; number <= 20; number++) {
-            System.out.println("Loop iteration:" + number);
+        for (int number = 0; number < 20; number++) {
+            System.out.println("number is " + number);
         }
-        System.out.println("The loop is complete.");
-        return;
     }
+//print the sum, part 3 of lab
+    private static int sumTwoNumbers(int a, int b) {
+        int sum = 10;
+        System.out.println("The sum of 3 and 7 is: " + sum);
+        return a + b;
+    }
+//part 4 of lab, loop counter to 10
+    private static int sumLoopCounter(int limit) {
+        int sum = 0;
+        for (int number = 1; number <= limit; number++) {
+            System.out.println("The sum of the loop counter is: " + sum);
+            sum = number;
+        }
+        return sum;
+    }
+//part 5 of lab, printing multiplication table, maybe use example of clock from slideshow?
+    private static void printMultiplicationTable() {
+        for (int n = 0; n <= 12; n++) {
+            System.out.println("Multiplication table for " + n);
 
-
-//step 3 of lab
-    private int sumTwoNumbers(int x, int y);
-
-
+        for (int m = 0; m <= 12; m++) {
+            System.out.println(n + " x " + m + " = " + (n * m));
+        }
+        }
+    }
 }

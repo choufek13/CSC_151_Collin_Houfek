d4fa04f09f3fd20591b5ee668d96fdceb0aa6f04
choufek13 <houfekcl700@mail.sandhills.edu>
2026-03-08 20:43:53 -0400

    lab 8 03/08

--- src/labs/example/arrays/ArrayOperations.java
+++ src/labs/example/arrays/ArrayOperations.java
@@ -6,34 +6,63 @@
 
 package labs/example.arrays;
 
+//create new array, part one of lab
 public class ArrayOperations {
-    public static void main(String[] args) {
-        createNewArray();
-        displayArray();
-        sortArray();
-        getDaysAndMonths();
-    }
-//part one of lab, create loop
-    public static void createNewArray(int 4) {
-
-    }
-
-    private static void displayArray() {
-
-    }
-
-//part 2 of lab
-    public static void sortArray() {
-
-    }
-
-//part 3 of lab, print out how many days each month has
-    public static void getDaysAndMonths() {
-        int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-        for (int index = 0; index < 12; index++)
-        {
-            System.out.println("Month " + (index + 1) + " has " + days[index] + " days.");
+    public void createNewArray(int baseValue) {
+        int[] newArray = new int[10];
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = baseValue + i;
         }
+    displayArray(newArray);
+    }
+
+//display array now
+    private void displayArray(int[] arr) {
+        System.out.println("I created a new array and it now has " + arr.length + " items in it.");
+        System.out.println("The array items and their values are listed below: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Index " + i + ": " + arr[i]);
+        }
+        System.out.println("------------------------------------------------");
+    }
+
+//part 2 of lab, sort array, use bubble-sort
+    public void sortArray(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    
+//print sorted array
+    System.out.println("Sorted Array Output (Bubble Sort):");
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i] + " ");
+    }
+    System.out.println("\n------------------------------------------------");
+    }
+
+//part 3 of lab, get days and months
+    public void getDaysAndMonths() {
+    //string array for months
+        String[] months = {
+            "January", "February", "March", "April", "May", "June", 
+            "July", "August", "September", "October", "November", "December"
+        };
+
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.println("Days in each month:");
+        
+        for (int i = 0; i < months.length; i++) {
+            System.out.println("There are " + days[i] + " days in " + months[i]);
+        }
+        System.out.println("------------------------------------------------");
     }
 }
--- src/labs/example/arrays/Main.java
+++ src/labs/example/arrays/Main.java
@@ -6,6 +6,28 @@
 
 package labs.example.arrays;
 
+import java.util.Random;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        ArrayOperations operations = new ArrayOperations();
+        System.out.println("--- Testing createNewArray ---");
+        operations.createNewArray(100);
+
+        System.out.println("--- Testing sortArray ---");
+        int[] unsortedArray = new int[50];
+        Random rand = new Random();
+
+        System.out.println("Unsorted Array (before method call):");
+        for (int i = 0; i < unsortedArray.length; i++) {
+            unsortedArray[i] = rand.nextInt(1000) + 1;
+            System.out.println(unsortedArray[i] + " ");
+        }
+        System.out.println("/n");
+
+        operations.sortArray(unsortedArray);
+
+        System.out.println("--- Testing getDaysAndMonths ---");
+        operations.getDaysAndMonths();
+    }
 }
